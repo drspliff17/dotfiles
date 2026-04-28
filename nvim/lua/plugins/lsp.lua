dcc414ddd33c4df63ce1a54774a194f5916f284b
drspliff17 <bluediamonditservice@gmail.com>
2026-04-28 11:44:12 +0100

    minor tweak to nvim/lua/plugins/lsp.lua
    added bashls entry to servers table (probably not even needed, i just
    cba to remove it right now)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,12 +1,16 @@
 return {
-   "neovim/nvim-lspconfig",
-   opts = {
-      servers = {
-         gdscript = {
-            capabilities = capabilities,
-            settings = {},
-            cmd = vim.fn.has("win32") == 1 and { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" } or nil,
-         },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      gdscript = {
+        capabilities = capabilities,
+        settings = {},
+        cmd = vim.fn.has("win32") == 1 and { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" } or nil,
       },
-   },
+      bashls = {
+        capabilities = capabilities,
+        settings = {},
+      },
+    },
+  },
 }
