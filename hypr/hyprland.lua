d76002365a002472a1db0729e473c9d2151d9124
drspliff17 <bluediamonditservice@gmail.com>
2026-05-01 20:05:05 +0100

    hypr/hyprland.lua
    tweaked require to modules format (untested)

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,14 +1,27 @@
 require("/home/drspliff/.cache/wal/colors-hyprland.conf")
 
-require("/home/drspliff/.config/hypr/new_conf/hyprland_keyword.lua")
-require("/home/drspliff/.config/hypr/new_conf/hyprland_autostart.lua")
-require("/home/drspliff/.config/hypr/new_conf/hyprland_env.lua")
-require("/home/drpliff/.config/hypr/new_conf/hyprland_permissions.lua")
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_keyword.lua")
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_autostart.lua")
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_env.lua")
+-- require("/home/drpliff/.config/hypr/new_conf/hyprland_permissions.lua")
+--
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_monitors.lua")
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_workspace_rules.lua")
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_window_rules.lua")
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_visuals.lua")
+--
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_input.lua")
+-- require("/home/drspliff/.config/hypr/new_conf/hyprland_keybindings.lua")
 
-require("/home/drspliff/.config/hypr/new_conf/hyprland_monitors.lua")
-require("/home/drspliff/.config/hypr/new_conf/hyprland_workspace_rules.lua")
-require("/home/drspliff/.config/hypr/new_conf/hyprland_window_rules.lua")
-require("/home/drspliff/.config/hypr/new_conf/hyprland_visuals.lua")
+require("modules.hyprland_keyword")
+require("modules.hyprland_autostart")
+require("modules.hyprland_env")
+require("modules.hyprland_permissions")
 
-require("/home/drspliff/.config/hypr/new_conf/hyprland_input.lua")
-require("/home/drspliff/.config/hypr/new_conf/hyprland_keybindings.lua")
+require("modules.hyprland_monitors")
+require("modules.hyprland_workspace_rules")
+require("modules.hyprland_window_rules")
+require("modules.hyprland_visuals")
+
+require("modules.hyprland_input")
+require("modules.hyprland_keybindings")
