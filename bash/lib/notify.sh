d40afc895432a15c822cedbbfe9c223a3aa10d16
drspliff17 <bluediamonditservice@gmail.com>
2026-05-15 11:18:11 +0100

    Additions to bash/
    Added bash/lib && ../notify.sh -> Place where "libs" will be stored,
    reusable helpers that im sick of rewriting basically
    
    Updated bash/scripts/syncMusicZoxide.sh to use bash/lib/notify.sh

---
+++ bash/lib/notify.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+#NOTE: Currently suited for short notifications, as when not TTY, notify-send gets passed the text as it's HEAD argument.
+#TODO: Find elegant solution for aforementioned limitation
+
+# Wrapper for sending some notification. Automatically decides whether to pass to stdout or notify-send. Handles own parsing.
+_notify() {
+
+  # Defaults
+  local d_urgency="low"
+  local d_time="1500"
+  local urgency time appname message
+  local doErr=0
+
+  # Helpers
+  _notify_tty() {
+    if [[ "$doErr" -eq 0 ]]; then
+      echo "$message"
+    else
+      echo "$message" >&2
+    fi
+  }
+
+  _notify_send() {
+    if [[ "$doErr" -eq 0 ]]; then
+      if [[ -z "$appname" ]]; then
+        notify-send -u "$urgency" -t "$time" "$message"
+      else
+        notify-send -u "$urgency" -t "$time" -a "$appname" "$message"
+      fi
+    else
+      if [[ -z "$appname" ]]; then
+        notify-send -u "$urgency" -t "$time" "Error" "$message"
+      else
+        notify-send -u "$urgency" -t "$time" -a "$appname" "Error" "$message"
+      fi
+    fi
+  }
+
+  while [[ "$#" -gt 1 ]]; do
+    case "$1" in
+    -u)
+      shift
+      urgency="$1"
+      shift
+      case "$urgency" in
+      low | normal | critical)
+        ;;
+      *)
+        urgency="$d_urgency"
+        ;;
+      esac
+      ;;
+    -t)
+      shift
+      time="$1"
+      shift
+      [[ ! "$time" =~ ^[0-9]+$ ]] && time="$d_time"
+      [[ ! "$time" -gt 0 ]] && time="$d_time"
+      ;;
+    -a)
+      shift
+      appname="$1"
+      shift
+      case "$appname" in
+      ct | center-text)
+        appname="center-text"
+        ;;
+      *)
+        appname=""
+        ;;
+      esac
+      ;;
+    -e | --error)
+      shift
+      doErr=1
+      ;;
+    esac
+  done
+  message="$1"
+  [[ -z "$message" ]] && return 1
+  if [[ -t 1 ]]; then
+    _notify_tty
+    return 0
+  else
+    [[ -z "$urgency" ]] && urgency="$d_urgency"
+    [[ -z "$time" ]] && time="$d_time"
+    _notify_send
+    return 0
+  fi
+}
--- bash/scripts/syncMusicZoxide.sh
+++ bash/scripts/syncMusicZoxide.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
+LIB_NOTFIY="$HOME/.config/bash/lib/notify.sh"
+source "$LIB_NOTFIY" >/dev/null || {
+  notify-send -a center-text -t 1500 -u critical "Error" "Failed to source lib: $LIB_NOTFIY" && exit 1
+}
 
 VERBOSE=0
-
 [[ "$1" = "-v" ]] && VERBOSE=1
 ROOT="$HOME/Music/Songs"
 ls "$ROOT" | while read line; do
-  zoxide query "$line"
+  zoxide query "$line" >/dev/null
   [[ "$?" -eq 1 ]] && {
     zoxide add "$line"
-    [[ $VERBOSE -eq 1 ]] && echo "Added $line to zoxide db"
+    [[ $VERBOSE -eq 1 ]] && _notify "Added $line to zoxide db"
   }
 done
-echo "[FINISHED]"
+_notify -a ct "Sync Complete"
