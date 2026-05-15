fab97c7bd93e025181b0754f4acdecf293037657
drspliff17 <bluediamonditservice@gmail.com>
2026-05-15 21:47:15 +0100

    Added fish/conf.d/aliases.fish -> rmmp3 = bash/scripts/fixYoutubeMusic_Duplicates.sh

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -8,6 +8,7 @@ alias nas="n ~/.config/fish/conf.d/aliases.fish"
 alias ssd="df -h | head -n 1; df -h | rg home; df -h | rg storage"
 alias fm="$HOME/.config/bash/scripts/fmp3.sh"
 alias sm="$HOME/.config/bash/scripts/syncPhoneMusic.sh -v"
+alias rmmp3="$HOME/.config/bash/scripts/fixYoutubeMusic_Duplicates.sh"
 
 # Builtin Shorthands
 alias Q="exit"
