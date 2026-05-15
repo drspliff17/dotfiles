a83cdce33b6213b38e803b2089a57e43adc73fa7
drspliff17 <bluediamonditservice@gmail.com>
2026-05-15 22:53:03 +0100

    Updated hypr/scripts/wofi_music_selector.sh to use bash/lib
    Now uses bash/lib/notify.sh && wofi_construct.sh

--- hypr/scripts/wofi_music_selector.sh
+++ hypr/scripts/wofi_music_selector.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-
 # Refactored dmenu_music_selector.sh, using Wofi
 
-#TODO: Cleanup
+LIB_NOTIFY="$HOME/.config/bash/lib/notify.sh"
+source "$LIB_NOTIFY" || {
+  notify-send -a center-text -t 1500 -u normal "Error" "Could not source required lib: $LIB_NOTIFY"
+  exit 1
+}
 
-#
-# Main switch statement control
-mode="$1"
-[[ -z "$mode" ]] && mode="files"
+LIB_WOFI="$HOME/.config/bash/lib/wofi_construct.sh"
+source "$LIB_WOFI" || {
+  _notify -a ct -e -u normal "Could not source required lib: $LIB_WOFI"
+  exit 1
+}
+
+mode="${1:-files}"
 
 # Root of search, when not using $cacheFile
 musicPath="$HOME/Music/Songs"
@@ -20,36 +26,10 @@ selectedArtist=""
 selectedFile=""
 selectedName=""
 
-# Passed to wofi, for -c
-w_prompt=""
-w_width="25%"
-w_height="50%"
-w_columns=""
-w_lines=""
+WOFI_WIDTH="25%"
+WOFI_HEIGHT="50%"
 
 w_args=()
-
-# Build wofi arguments (w_args) from w_* variables
-_constructArgs() {
-  w_args=()
-
-  w_args+=("--prompt" "$w_prompt")
-  w_args+=("--width" "$w_width")
-  w_args+=("--height" "$w_height")
-
-  [[ -n $w_columns ]] && w_args+=("--columns" "$w_columns")
-  [[ -n $w_lines ]] && w_args+=("--lines" "$w_lines")
-}
-
-# Simple Wrapper for shortening notify-send, args: appname  urgency  head  body
-_notify() {
-  local appname="$1"
-  local urg="$2"
-  local head="$3"
-  local body="$4"
-  notify-send -u "$urg" -t 2000 -a "$appname" "$head" "$body"
-  return 0
-}
 
 # Updates $cacheFile with new-line entries
 _updateMusicSelectorCache() {
@@ -74,8 +54,8 @@ _menuFromCache() {
   [[ -f "$cacheFile" ]] || _updateMusicSelectorCache
   local selection fullpath
 
-  w_prompt="Cached Music"
-  _constructArgs
+  WOFI_PROMPT="Cached Music"
+  _construct w_args
   selection="$(
     awk -F'|' -v base="$musicPath" '{
     title=$1
@@ -111,28 +91,28 @@ _menuFromCache() {
   }' "$cacheFile"
   )"
 
-  [[ -z "$fullpath" ]] && _notify center-text normal "Invalid Selection" && exit 1
+  [[ -z "$fullpath" ]] && _notify -a ct -u normal -e "Invalid Selection" && exit 1
   selectedFile="$fullpath"
   return 0
 }
 
 # Generate wofi from $musicPath (Set selectedArtist)
 _menuArtistSelection() {
-  w_prompt="Select Artist"
-  _constructArgs
+  WOFI_PROMPT="Select Artist"
+  _construct w_args
   selectedArtist="$(/usr/bin/ls "$musicPath" | sed 's/_/ /g' | wofi -d "${w_args[@]}")"
   [[ -z "$selectedArtist" ]] && return 1
-  w_prompt="$selectedArtist"
+  WOFI_PROMPT="$selectedArtist"
   selectedArtist="$(echo $selectedArtist | sed 's/ /_/g')"
 
-  [[ ! -d "$musicPath/$selectedArtist" ]] && _notify center-text normal "Invalid Artist" && exit 1
+  [[ ! -d "$musicPath/$selectedArtist" ]] && _notify -a ct -u normal -e "Invalid Artist" && exit 1
   return 0
 }
 
 # Generate wofi from $musicPath/$selectedArtist (Sets selectedFile)
 _menuArtistFileSelection() {
   [[ -z "$selectedArtist" ]] && return 1
-  _constructArgs
+  _construct w_args
   selectedFile="$(/usr/bin/ls "$musicPath/$selectedArtist" | sed 's/_/ /g' | sed 's/\.[^.]*$//' | wofi -d "${w_args[@]}")"
   [[ -z "$selectedFile" ]] && exit 1
   selectedArtist="$(echo "$selectedArtist" | sed 's/_/ /g')"
@@ -140,12 +120,11 @@ _menuArtistFileSelection() {
 
   selectedArtist="$(echo "$selectedArtist" | sed 's/ /_/g')"
   selectedFile="$(echo $selectedFile | sed 's/ /_/g').mp3"
-  [[ ! -f "$musicPath/$selectedArtist/$selectedFile" ]] && _notify center-text normal "Invalid Song" && exit 1
+  [[ ! -f "$musicPath/$selectedArtist/$selectedFile" ]] && _notify -a ct -u normal -e "Invalid Song" && exit 1
   return 0
 }
 
 # Main
-
 case "$mode" in
 artist | files)
   if ! pgrep cmus; then
@@ -160,14 +139,14 @@ artist)
   _menuArtistSelection
   _menuArtistFileSelection
   [[ -z "$selectedArtist" || -z "$selectedFile" ]] && exit 1
-  cmus-remote -f "$musicPath/$selectedArtist/$selectedFile" && _notify center-text low "Now Playing: $selectedName"
+  cmus-remote -f "$musicPath/$selectedArtist/$selectedFile" && _notify -a ct -t 2000 "Now Playing: $selectedName"
   exit 0
   ;;
 
 files)
   _menuFromCache
   [[ -z "$selectedFile" ]] && exit 1
-  cmus-remote -f "$selectedFile" && _notify center-text low "Now Playing: $selectedName"
+  cmus-remote -f "$selectedFile" && _notify -a ct -t 2000 "Now Playing: $selectedName"
   exit 0
   ;;
 
@@ -176,12 +155,13 @@ update)
     cmus-remote -C 'add Music'
     cmus-remote -C 'update-cache'
   fi
-  _notify center-text low "Starting Cache Update..." && _updateMusicSelectorCache && _notify center-text low "Cache Updated: $cacheFile"
+  _notify -a ct "Starting Cache Update..." && _updateMusicSelectorCache && _notify -a ct "Cache Updated: $cacheFile"
   exit 0
   ;;
 
 *)
-  _notify center-text urgent "[ERROR] wofi_music_selector.sh" " Invalid mode! Valid = [ artist files update ] | Got: $mode "
+  # _notify center-text urgent "[ERROR] wofi_music_selector.sh" " Invalid mode! Valid = [ artist files update ] | Got: $mode "
+  _notify -a ct -u urgent "[ERROR] wofi_music_selector.sh" " Invalid mode! Valid = [ artist files update ] | Got: $mode "
   exit 1
   ;;
 esac
