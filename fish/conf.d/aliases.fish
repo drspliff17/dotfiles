5f80a5f9388a1f708047ac548cb944f450bb6734
drspliff17 <bluediamonditservice@gmail.com>
2026-05-15 12:09:57 +0100

    Added fish/conf.d/aliases.fish -> fm = bash/scripts/fmp3.sh

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -6,6 +6,7 @@ alias nfrc="n ~/.config/fish/config.fish"
 alias nbrc="n ~/.bashrc"
 alias nas="n ~/.config/fish/conf.d/aliases.fish"
 alias ssd="df -h | head -n 1; df -h | rg home; df -h | rg storage"
+alias fm="$HOME/.config/bash/scripts/fmp3.sh"
 
 # Builtin Shorthands
 alias Q="exit"
