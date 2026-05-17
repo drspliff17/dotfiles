7ce73cd90a928e0b61d433550cbe6111ccb6a58f
drspliff17 <bluediamonditservice@gmail.com>
2026-05-17 18:41:23 +0100

    aliases.fish added ff -> fastfetch

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -6,6 +6,7 @@ alias nfrc="n ~/.config/fish/config.fish"
 alias nbrc="n ~/.bashrc"
 alias nas="n ~/.config/fish/conf.d/aliases.fish"
 alias ssd="df -h | head -n 1; df -h | rg home; df -h | rg storage"
+alias ff="fastfetch"
 
 #Music stuff because am lazy ja
 alias fm="$HOME/.config/bash/scripts/fmp3.sh"
