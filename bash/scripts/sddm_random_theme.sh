a4518da7a1f0e728240428c0de6029b5b3c9d7b3
drspliff17 <bluediamonditservice@gmail.com>
2026-05-19 13:27:10 +0100

    Updated bash/scripts/sddm_random_theme.sh
    Filters out Current, prior to assigning new theme, if possible.

--- bash/scripts/sddm_random_theme.sh
+++ bash/scripts/sddm_random_theme.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-SDDM_THEME_DIR="/usr/share/sddm/themes"
-C_FILE="/etc/sddm.conf.d/sddm.conf"
-THEMES=($(ls -i "$SDDM_THEME_DIR"))
-R_THEME=$(find "$SDDM_THEME_DIR" -mindepth 1 -maxdepth 1 -type d -printf "%f\n" | shuf -n 1)
-cat <<EOF >"$C_FILE"
+THEME_DIR="/usr/share/sddm/themes"
+CONFIG_FILE="/etc/sddm.conf.d/sddm.conf"
+
+CURRENT_THEME=$(grep -Po '^Current=\K.*' "$CONFIG_FILE" 2>/dev/null)
+
+mapfile -t THEMES < <(
+  find "$THEME_DIR" -mindepth 1 -maxdepth 1 -type d -printf "%f\n"
+)
+
+FILTERED=()
+for theme in "${THEMES[@]}"; do
+  if [[ "$theme" != "$CURRENT_THEME" ]]; then
+    FILTERED+=("$theme")
+  fi
+done
+
+if [ ${#FILTERED[@]} -eq 0 ]; then
+  FILTERED=("${THEMES[@]}")
+fi
+
+RANDOM_THEME="${FILTERED[RANDOM % ${#FILTERED[@]}]}"
+cat >"$CONFIG_FILE" <<EOF
 [Theme]
-Current=$R_THEME
+Current=$RANDOM_THEME
 EOF
