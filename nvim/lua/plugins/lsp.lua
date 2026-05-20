095612ee817834504629984eb9ffaf85a1acfa32
drspliff17 <bluediamonditservice@gmail.com>
2026-05-20 23:53:42 +0100

    nvim/lua/plugins/lsp.lua Added qmlls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,6 +7,7 @@ return {
         settings = {},
         cmd = vim.fn.has("win32") == 1 and { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" } or nil,
       },
+
       bashls = {
         capabilities = capabilities,
         settings = {},
@@ -21,6 +22,17 @@ return {
                 vim.fn.expand("~/.local/share/hyprland-meta/hl.meta.lua"),
               },
               checkThirdParty = false,
+            },
+          },
+        },
+      },
+      qmlls = {
+        cmd = { "qmlls", "-E" },
+        filetypes = { "qml", "qmljs" },
+        settings = {
+          qmlls = {
+            importPaths = {
+              "/usr/lib/qt6/qml",
             },
           },
         },
