fe5ec82dd90089dc9ebdc40f4762a3de44e42bc8
drspliff17 <bluediamonditservice@gmail.com>
2026-06-14 09:55:27 +0100

    wip mako && bash/lib/notify.sh

--- bash/lib/notify.sh
+++ bash/lib/notify.sh
@@ -78,6 +78,15 @@ _notify() {
       ts | theme_selector)
         appname="theme_selector"
         ;;
+      vnd | view-notification-details)
+        appname="view-notification-details"
+        ;;
+      nh | no-history)
+        appname="no-history"
+        ;;
+      nhc | nh-center-text)
+        appname="nh-center-text"
+        ;;
       *)
         appname=""
         ;;
@@ -95,6 +104,9 @@ _notify() {
   done
   message="$1"
   [[ -z "$message" ]] && return 1
+
+  # TODO: Add custom parsing module here
+
   if [[ -t 1 ]]; then
     _notify_tty
     return 0
@@ -105,3 +117,92 @@ _notify() {
     return 0
   fi
 }
+
+#TODO: Make status head injection thing, for which -e / --error will be a shorthand of effectively -s err or something
+# _nnotify() {
+#   local def_urgency="low" def_time=1500 doError=false
+#   local n_args=()
+#   local head body message
+#
+#   local n_appnames=()
+#   mapfile -t n_appnames < <(rg app-name= ~/.config/mako/config | cut -d= -f2 | tr -d ']' | sed 's/"//g')
+#
+#   _addArg() {
+#     n_args+=("$1 $2")
+#   }
+#
+#   _validAppname() {
+#     for name in "${n_appnames[@]}"; do
+#       [[ "$name" = "$1" ]] && _addArg "-a" "$1" && return 0
+#       case "$1" in
+#       ct) _addArg "-a" "center-text" && return 0 ;;
+#       ts) _addArg "-a" "theme_selector" && return 0 ;;
+#       vnd) _addArg "-a" "view-notification-details" && return 0 ;;
+#       nh) _addArg "-a" "no-history" && return 0 ;;
+#       nhc) _addArg "-a" "nh-center-text" && return 0 ;;
+#       esac
+#     done
+#     return 1
+#   }
+#
+#   while [[ "$#" -gt 1 ]]; do
+#     case "$1" in
+#     -u | --urgency)
+#       case "$2" in
+#       l | low)
+#         _addArg "-u" "low"
+#         ;;
+#       n | normal)
+#         _addArg "-u" "normal"
+#         ;;
+#       c | critical)
+#         _addArg "-u" "critical"
+#         ;;
+#       *)
+#         _addArg "-u" "$def_urgency"
+#         ;;
+#       esac
+#       shift 2
+#       ;;
+#     -t | --time)
+#       [[ ! "$2" =~ ^[0-9]+$ ]] && _addArg "-t" "$def_time" && shift 2 && continue
+#       [[ "$2" -le 0 ]] && _addArg "-t" "$def_time" && shift 2 && continue
+#       _addArg "-t" "$2"
+#       shift 2
+#       ;;
+#     -a | --appname)
+#       if ! _validAppname "$2"; then
+#         notify-send -u normal -a center-text "Invalid appname given, skipping inclusion." "Valid names are: [${n_appnames[*]}]"
+#         shift 2
+#         continue
+#       fi
+#       # _addArg "-a" "$2"
+#       shift 2
+#       ;;
+#     -i | --icon)
+#       _addArg "-i" "$2"
+#       shift 2
+#       ;;
+#     -n | --app-icon)
+#       _addArg "-n" "$2"
+#       shift 2
+#       ;;
+#     -e | --error)
+#       doError=true
+#       shift
+#       ;;
+#     esac
+#   done
+#   # Only like this for compat with prev logic
+#   head="$1"
+#   shift
+#
+#   if [[ -t 1 ]]; then
+#     $doError && head="ERROR $head"
+#     echo "$head" && return 0
+#   else
+#     $doError && head="[ERROR] $head"
+#     notify-send "${n_args[@]}" "$head"
+#   fi
+#
+# }
