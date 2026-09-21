2e20b6ee29f3b16fde16ca5ab936e43300f7c0cc
drspliff17 <bluediamonditservice@gmail.com>
2026-09-21 10:03:28 +0100

    removed old dunst scripts from hypr/scripts

--- hypr/scripts/dunst_history_clear.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-notify-send -u low -t 1000 -a center-text "Notification History Cleared!"
-sleep 1
-dunstctl history-clear
--- hypr/scripts/dunst_history_get.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-nString="
-Waiting: $(dunstctl count waiting)
-History: $(dunstctl count history)
-"
-notify-send -u low -t 2500 "Notifications Status" "$nString"
